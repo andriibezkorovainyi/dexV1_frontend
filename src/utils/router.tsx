900b93da9873cf8e886509f54ad69901c89919c3
Andrii Bezkorovainyi <crm29865@gmail.com>
2023-08-11 18:57:10 +0300

    prepare for deploy

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,11 +1,11 @@
-import {createBrowserRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
+import {createBrowserRouter, createHashRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
 import {Swap} from "../pages/Swap/Swap";
 import {MainLayout} from "../layouts/MainLayout";
 import {Liquidity} from "../pages/Liquidity/Liquidity";
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
     createRoutesFromElements(
-        <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<MainLayout />}>
             <Route index element={<Navigate to={'swap'} replace />} />
             <Route path='swap' element={<Swap />} />
             <Route path='liquidity' element={<Liquidity />} />
